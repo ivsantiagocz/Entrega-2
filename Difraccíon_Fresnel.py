ec73e4ba684136d81a85333ace25335266f2da34
ivsantiagocz <43282648+ivsantiagocz@users.noreply.github.com>
2021-11-19 04:56:19 -0500

    Update Difraccíon_Fresnel.py

--- Difraccíon_Fresnel.py
+++ Difraccíon_Fresnel.py
@@ -17,7 +17,7 @@ wl= 633e-6
 L=0.5 #medida en milimetros del objeto
 N= 1024  #numero de particiones del objeto
 dx= L/N #tamaño de particiones del objeto
-nf=3
+nf=4
 
 
 radio = 0.05
@@ -30,30 +30,25 @@ hh= np.zeros((N,N))*np.exp(-1j*0)
 
 k=(2*np.pi)/wl
 
-#if (z>=(N*(dx)**2)/wl):
-print("z está en el dominio de aplicación de DD de FF")
+
 for t in range(int(-N/2),int(N/2)):
             for s in range(int(-N/2),int(N/2)):
     
                 h= np.exp(1j*k*((dx*t)**2 + (dx*s)**2)/(2*z))
                 hh[t,s]=h 
 
-h1=np.fft.fftshift(hh)
-for t in range(int(-N/2),int(N/2)):
-        for s in range(int(-N/2),int(N/2)):
-                            
-            Ut[t,s] = ut[t,s]*h1[t,s]
-            
-U1=np.fft.fft2(Ut)
-Uf=np.fft.fftshift(U1)
+h1=np.fft.fft2(hh)
+
+uf=np.fft.fft2(ut)
+Ut=uf*h1            
+Uf=np.fft.ifft2(Ut)
+#Uf=np.fft.fftshift(U1)
 U=abs(Uf*Uf)
 
 #I = Image.fromarray(abs(U), 'P')
 plt.figure(figsize=(7,4))
-im1 = plt.imshow(np.angle(h1),cmap='gray',extent=(-L/2,L/2,-L/2,L/2))
-im2 = plt.imshow(U,cmap='gray')
-plt.ylabel('mm')
-plt.xlabel('mm')
-plt.colorbar(im1,shrink=0.5)
+#im1 = plt.imshow(np.angle(h1),cmap='gray',extent=(-L/2,L/2,-L/2,L/2))
+plt.imshow(U,cmap='gray')
+
 plt.show()
 
